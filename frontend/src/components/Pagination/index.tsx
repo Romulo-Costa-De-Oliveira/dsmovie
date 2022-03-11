24405e37280ff3d6e5a4858042d8b582d684342b
Romulo Costa <romulocoliveirag@gmail.com>
2022-03-11 06:40:12 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -6,11 +6,11 @@ function Pagination(){
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
                 <button className="dsmovie-pagination-button" disabled={true} >
-                    <Arrow className="dsmovie-flip-horizontal"/>
+                    <Arrow />
                 </button>
                 <p>{`${1} de ${3}`}</p>
                 <button className="dsmovie-pagination-button" disabled={false} >
-                    <Arrow />
+                    <Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
         </div>
